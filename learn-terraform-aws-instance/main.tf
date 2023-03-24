b663d9fec675414eb6abdb48af7dcefffae6a643
Terrel Brinkley <103070295+TerrelBrinkley@users.noreply.github.com>
2023-03-24 01:41:52 -0400

    Terraform Practice - AWS Init

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      vversion = "~> 4.16"
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
-}
-  required_version = ">= 1.20"
+  }
+  required_version = ">= 1.4.2"
 }
 
 provider "aws" {
   region = "us-east-2"
-  
+
 }
 
 resource "aws_instance" "app_server" {
-  ami = ami-02f97949d306b597a
+  ami           = ami-02f97949d306b597a
   instance_type = "t2-micro"
 
   tags = {
